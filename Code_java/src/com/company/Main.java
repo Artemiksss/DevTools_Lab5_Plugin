a29905e483aaf07a5e89c7d470dcd01a6550c94d
Artemiksss <artem.zhovnir@yandex.ru>
2022-12-11 23:33:22 +0300

    Add new file

---
+++ Code_java/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+        public static void main(String[] args) {
+            int i = (int) (Math.random() * 10 + 1);
+            System.out.println("Случайное число:" + i);
+
+        }
+}
+
---
+++ Search_Plugin/src/main/java/New_pr.java
@@ -0,0 +1,40 @@
+
+
+import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.ui.Messages;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class New_pr extends AnAction {
+    @Override
+    public void actionPerformed(AnActionEvent event) {
+        Editor editor = event.getData(PlatformDataKeys.EDITOR);
+
+        String selectedText = editor.getSelectionModel().getSelectedText();
+
+        if (selectedText != null) {
+            String Highlighted = "";
+            try {
+                Highlighted = URLEncoder.encode(selectedText, StandardCharsets.UTF_8.toString());
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+            String url = String.format("https://yandex.ru/search/?text=%s&search_source=dzen_desktop_safe&lr=2", Highlighted);
+            BrowserUtil.browse(url);
+        } else {
+            Messages.showMessageDialog("You have not highlighted the text.", "Tweet Action", Messages.getInformationIcon());
+        }
+    }
+
+    @Override
+    public boolean isDumbAware() {
+        return false;
+    }
+}
